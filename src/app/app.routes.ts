6836218abdec67e9a39ea0bc19061cfec904bca0
Admin <Admin@DESKTOP-40H717B>
2025-06-12 15:23:25 +0700

    - Update menu - Update login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
-import { AppComponent } from './app.component';
 import { ProjectComponent } from './pages/project/project.component';
+import { LoginComponent } from './auth/login/login.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
-  { path: 'app', component: AppComponent },
-  { path: 'welcome', component: WelcomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
 
   // Router danh mục dự án
+  { path: 'welcome', component: WelcomeComponent },
   { path: 'project', component: ProjectComponent }, // Dự án master
 ];

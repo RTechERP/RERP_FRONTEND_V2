63ff3816c170af65a4fe58bfaf37e3dc7002c8d2
namvu <namvu@NAM>
2025-06-12 15:13:54 +0700

    Update router dự án

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
 import { AppComponent } from './app.component';
 import { ProjectComponent } from './pages/project/project.component';
+import { ProjectListWorkReportComponent } from './pages/project/project-list-work-report/project-list-work-report.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/welcome', pathMatch: 'full' },
@@ -10,4 +11,5 @@ export const routes: Routes = [
 
   // Router danh mục dự án
   { path: 'project', component: ProjectComponent }, // Dự án master
+  { path: 'projectListWork/:id', component: ProjectListWorkReportComponent }, // Dự án master
 ];

2c39c2e9ebddea512ee7a63fcce4361ce46d7acf
Admin <Admin@DESKTOP-40H717B>
2025-09-27 16:48:38 +0700

    update style khách hang, tabulator - 27/09/2025

--- src/app/tabulator-default.config.ts
+++ src/app/tabulator-default.config.ts
@@ -1,32 +1,33 @@
-import {TabulatorFull as Tabulator, Options} from 'tabulator-tables';
+import { TabulatorFull as Tabulator, Options } from 'tabulator-tables';
 
 export const DEFAULT_TABLE_CONFIG: Options = {
-    layout: 'fitDataFill',
-    height: '89vh',
-    pagination: true,
-    paginationSize: 50,
-    paginationSizeSelector: [10, 30, 50, 100, 300, 500],
-    movableColumns: true,
-    resizableRows: true,
-    reactiveData: true,
-    langs: {
-        vi: {
-          pagination: {
-            first: '<<',
-            last: '>>',
-            prev: '<',
-            next: '>',
-          },
-        },
+  layout: 'fitDataFill',
+  height: '85vh',
+  pagination: true,
+  paginationSize: 50,
+  paginationSizeSelector: [10, 30, 50, 100, 300, 500],
+  paginationMode: 'remote',
+  movableColumns: true,
+  resizableRows: true,
+  reactiveData: true,
+  langs: {
+    vi: {
+      pagination: {
+        first: '<<',
+        last: '>>',
+        prev: '<',
+        next: '>',
       },
-    locale: 'vi',
-    columnDefaults: {
-        headerWordWrap: true,
-        headerVertical: false,
-        headerHozAlign: "center",
-        minWidth: 60,
-        hozAlign: "left",
-        vertAlign: 'middle',
-        resizable: true
     },
-  };
+  },
+  locale: 'vi',
+  columnDefaults: {
+    headerWordWrap: true,
+    headerVertical: false,
+    headerHozAlign: 'center',
+    minWidth: 60,
+    hozAlign: 'left',
+    vertAlign: 'middle',
+    resizable: true,
+  },
+};

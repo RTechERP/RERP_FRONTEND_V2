0dc63d5fd5d5ac14ae35a7aeccf7870f21cef06e
Admin <Admin@DESKTOP-40H717B>
2025-10-22 08:53:25 +0700

    Merge branch 'XuanLuong'

--- src/app/pages/old/ts-asset-management-personal/ts-asset-management-personal.component.ts
+++ src/app/pages/old/ts-asset-management-personal/ts-asset-management-personal.component.ts
@@ -1,4 +1,9 @@
-import { AfterViewInit, Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { FormsModule } from '@angular/forms';
 import { NzButtonModule, NzButtonSize } from 'ng-zorro-antd/button';
@@ -50,68 +55,97 @@ import { TsAssetManagementPersonalFormComponent } from './ts-asset-management-pe
     NzTabsModule,
   ],
   templateUrl: './ts-asset-management-personal.component.html',
-  styleUrls: ['./ts-asset-management-personal.component.css']
+  styleUrls: ['./ts-asset-management-personal.component.css'],
 })
-export class TsAssetManagementPersonalComponent implements OnInit, AfterViewInit {
+export class TsAssetManagementPersonalComponent
+  implements OnInit, AfterViewInit
+{
   assetManagemnetPersonalData: any[] = [];
-   private ngbModal = inject(NgbModal);
+  private ngbModal = inject(NgbModal);
   tableAssetManagementPersonal: Tabulator | null = null;
-  constructor(private tsAssetmanagementPersonal: TsAssetManagementPersonalService,
-      private notification: NzNotificationService,
-       private nzModal: NzModalService,      
-       private tsAssetAllocationPersonalService: TsAssetAllocationPersonalService,
-  ) { }
+  constructor(
+    private tsAssetmanagementPersonal: TsAssetManagementPersonalService,
+    private notification: NzNotificationService,
+    private nzModal: NzModalService,
+    private tsAssetAllocationPersonalService: TsAssetAllocationPersonalService
+  ) {}
   ngOnInit() {
     this.getAssetManagementPersonal();
   }
   ngAfterViewInit(): void {
     this.getAssetManagementPersonal();
     this.drawTableAssetManagementPersonal();
-
   }
   getAssetManagementPersonal() {
-    this.tsAssetmanagementPersonal.getAssetsManagementPersonal().subscribe((data: any) => {
-      this.assetManagemnetPersonalData = data.tSAssetManagmentPersonal;
-      this.tableAssetManagementPersonal?.setData(data.tSAssetManagmentPersonal);
-      console.log('Data:', this.assetManagemnetPersonalData)
-    });
-  }
-  drawTableAssetManagementPersonal(): void {
-    this.tableAssetManagementPersonal = new Tabulator('#dataTableAssetManagementPersonal',
-      {
-...DEFAULT_TABLE_CONFIG,
-paginationMode:"local",
-        columns: [
-          { title: 'STT', field: 'STT', hozAlign: 'right', width: 70, headerHozAlign: 'center' },
-          { title: 'ID', field: 'ID', hozAlign: 'right', width: 70, headerHozAlign: 'center' , visible:false},
-                    { title: 'Mã tài sản', field: 'Code', headerHozAlign: 'center'},
-                          { title: 'Tên tài sản', field: 'Name', headerHozAlign: 'center'},
-
-          { title: 'Đơn vị tính', field: 'UnitName', headerHozAlign: 'center'},
-          { title: 'Số lượng trong kho', field: 'RemainingQuantity', headerHozAlign: 'center', hozAlign: 'right' },
-     {
-  title: 'Ngày mua',
-  field: 'DateBuy',
-  hozAlign: 'left',
-  formatter: (cell) => {
-    const value = cell.getValue();
-    if (!value) return '';
-    const date = DateTime.fromISO(value);
-    return date.isValid ? date.toFormat('dd/MM/yyyy') : '';
-  }
-},
-           { title: 'Note', field: 'Note', hozAlign: 'left', width:300 }
-        ]
+    this.tsAssetmanagementPersonal
+      .getAssetsManagementPersonal()
+      .subscribe((data: any) => {
+        this.assetManagemnetPersonalData = data.tSAssetManagmentPersonal;
+        this.tableAssetManagementPersonal?.setData(
+          data.tSAssetManagmentPersonal
+        );
+        console.log('Data:', this.assetManagemnetPersonalData);
       });
   }
+  drawTableAssetManagementPersonal(): void {
+    this.tableAssetManagementPersonal = new Tabulator(
+      '#dataTableAssetManagementPersonal',
+      {
+        ...DEFAULT_TABLE_CONFIG,
+        paginationMode: 'local',
+        columns: [
+          {
+            title: 'STT',
+            field: 'STT',
+            hozAlign: 'right',
+            width: 70,
+            headerHozAlign: 'center',
+          },
+          {
+            title: 'ID',
+            field: 'ID',
+            hozAlign: 'right',
+            width: 70,
+            headerHozAlign: 'center',
+            visible: false,
+          },
+          { title: 'Mã tài sản', field: 'Code', headerHozAlign: 'center' },
+          { title: 'Tên tài sản', field: 'Name', headerHozAlign: 'center' },
 
-addAssetPersonal() {
-    const modalRef = this.ngbModal.open(TsAssetManagementPersonalFormComponent, {
-      size: 'xl',
-      backdrop: 'static',
-      keyboard: false,
-      centered: true,
-    });
+          { title: 'Đơn vị tính', field: 'UnitName', headerHozAlign: 'center' },
+          {
+            title: 'Số lượng trong kho',
+            field: 'RemainingQuantity',
+            headerHozAlign: 'center',
+            hozAlign: 'right',
+          },
+          {
+            title: 'Ngày mua',
+            field: 'DateBuy',
+            hozAlign: 'left',
+            formatter: (cell) => {
+              const value = cell.getValue();
+              if (!value) return '';
+              const date = DateTime.fromISO(value);
+              return date.isValid ? date.toFormat('dd/MM/yyyy') : '';
+            },
+          },
+          { title: 'Ghi chú', field: 'Note', hozAlign: 'left', width: 300 },
+        ],
+      }
+    );
+  }
+
+  addAssetPersonal() {
+    const modalRef = this.ngbModal.open(
+      TsAssetManagementPersonalFormComponent,
+      {
+        size: 'xl',
+        backdrop: 'static',
+        keyboard: false,
+        centered: true,
+      }
+    );
     //     modalRef.componentInstance.dataInput =null;
     modalRef.result.then(
       (result) => {
@@ -123,26 +157,31 @@ addAssetPersonal() {
     );
   }
   editAssetPersonal() {
-  const selected = this.tableAssetManagementPersonal?.getSelectedData() || [];
-  if (!selected.length) {
-    this.notification.warning('Thông báo', 'Vui lòng chọn một dòng để sửa!');
-    return;
+    const selected = this.tableAssetManagementPersonal?.getSelectedData() || [];
+    if (!selected.length) {
+      this.notification.warning('Thông báo', 'Vui lòng chọn một dòng để sửa!');
+      return;
+    }
+    const rowData = { ...selected[0] };
+
+    const modalRef = this.ngbModal.open(
+      TsAssetManagementPersonalFormComponent,
+      {
+        size: 'xl',
+        backdrop: 'static',
+        keyboard: false,
+        centered: true,
+      }
+    );
+
+    modalRef.componentInstance.dataInput = rowData;
+
+    modalRef.result.then(
+      () => this.getAssetManagementPersonal(),
+      () => {}
+    );
   }
-  const rowData = { ...selected[0] };
-
-  const modalRef = this.ngbModal.open(TsAssetManagementPersonalFormComponent, {
-    size: 'xl', backdrop: 'static', keyboard: false, centered: true
-  });
-
-
-  modalRef.componentInstance.dataInput = rowData;
-
-  modalRef.result.then(
-    () => this.getAssetManagementPersonal(),
-    () => {}
-  );
-}
-deleteAssetPersonal() {
+  deleteAssetPersonal() {
     const selected = this.tableAssetManagementPersonal?.getSelectedData() || [];
     if (selected.length !== 1) {
       this.notification.warning('Thông báo', 'Chọn đúng một dòng để xóa');
@@ -157,10 +196,11 @@ deleteAssetPersonal() {
       nzOkDanger: true,
       nzCancelText: 'Hủy',
       nzOnOk: () => {
-
-        const payload = { tSAssetManagementPersonal: { id: ID, isDeleted: true } };
+        const payload = {
+          tSAssetManagementPersonal: { id: ID, isDeleted: true },
+        };
         return this.tsAssetAllocationPersonalService
-          .saveAssetAllocationPerson(payload)   
+          .saveAssetAllocationPerson(payload)
           .toPromise()
           .then((res: any) => {
             if (res?.status === 1) {
@@ -175,4 +215,3 @@ deleteAssetPersonal() {
     });
   }
 }
-

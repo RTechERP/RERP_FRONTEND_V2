6d9bc1d681dcc4f21a4e404378800da111a521cd
TuNam <vutunam.rtc@gmail.com>
2025-11-21 09:05:53 +0700

    Update bổ sung chức năng nhà cung cấp - module mua hàng

--- src/app/pages/purchase/supplier-sale/supplier-sale.service.ts
+++ src/app/pages/purchase/supplier-sale/supplier-sale.service.ts
@@ -13,6 +13,9 @@ export class SupplierSaleService {
 
   private apiUrl = environment.host + 'api/SupplierSale/';
   private apiUrlProject = environment.host + 'api/Project/';
+  private apiUrlTaxCompany = environment.host + 'api/TaxCompany/';
+  private apiUrlSupplierSaleContact = environment.host + 'api/SupplierSaleContact/';
+  private apiUrlRulePay = environment.host + 'api/RulePay/';
 
   constructor(
     private http: HttpClient,
@@ -20,18 +23,13 @@ export class SupplierSaleService {
   ) { }
 
   // Danh sách sale NCC
-  getSupplierSaleByID(supplierSaleID: number): Observable<any> {
-    return this.http.get<any>(
-      this.apiUrl + `getsalesupplierbyid?supplierID=${supplierSaleID}`,
-    );
-  }
   getSupplierSale() {
     return this.apiUrl + `supplier-sale`;
   }
   // Danh sách sale supplier sale contact
   getSupplierSaleContact(supplierID: number): Observable<any> {
     return this.http.get<any>(
-      this.apiUrl + `supplier-sale-contact?supplierID=${supplierID}`,
+      this.apiUrlSupplierSaleContact + `supplier-sale-contact?supplierID=${supplierID}`,
     );
   }
   // get project employee
@@ -40,97 +38,12 @@ export class SupplierSaleService {
       this.apiUrlProject + `get-project-employee/${status}`
     );
   }
-  // get document sale admin
-  getDocumentSaleAdmin(departmentID: number): Observable<any> {
-    return this.http.get<any>(
-      this.apiUrl + `getdocumentsaleadmin/${departmentID}`
-    );
-  }
-  // get documentfile
-  getDocumentFile(documentID: number): Observable<any> {
-    return this.http.get<any>(
-      this.apiUrl + `getdocumentfile/${documentID}`
-    );
-  }
-
-  // get documentfile
-  getDocumentType(): Observable<any> {
-    return this.http.get<any>(
-      this.apiUrl + `getdocumenttype/`
-    );
-  }
-  // get document by id
-  getDocument(documentID: number): Observable<any> {
-    return this.http.get<any>(
-      this.apiUrl + `getdocument/${documentID}`
-    );
-  }
-
-  // get rule pay
-  getRulePay(): Observable<any> {
-    return this.http.get<any>(
-      this.apiUrl + `getrulepay`
-    );
-  }
-  // get department
-  getDepartment(): Observable<any> {
-    return this.http.get<any>(
-      this.apiUrl + `getdepartment`
-    );
-  }
   // get tax company
   getTaxCompany(): Observable<any> {
     return this.http.get<any>(
-      this.apiUrl + `gettaxcompany`
+      this.apiUrlTaxCompany + `tax-company`
     );
   }
-  // check exist code document
-  getCheckExistDocument(code: any): Observable<any> {
-    return this.http.get<any>(
-      this.apiUrl + `getcheckexistdocument?code=${code}`
-    );
-  }
-  // tải xuống document file
-  downloadDocumentFile(
-    arrDocumentFileID: number[],
-    documentType: string,
-    departmentCode: string
-  ) {
-    const form = new FormData();
-    form.append('departmentCode', departmentCode);
-    form.append('documentType', documentType);
-    arrDocumentFileID.forEach(id => form.append('arrDocumentFileID', String(id)));
-
-
-    return this.http.post<any>(
-      this.apiUrl + 'downloaddocumentfile',
-      form
-    );
-  }
-
-  // đẩy file lên server và lưu document file
-  uploadDocumentFiles(
-    departmentCode: string,
-    documentType: string,
-    documentID: number,
-    files: File[]
-  ) {
-    const form = new FormData();
-    form.append('departmentCode', departmentCode);
-    form.append('documentType', documentType);
-    form.append('documentID', documentID.toString());
-
-    files.forEach(file => {
-      form.append('files', file, file.name); // 'files' trùng với List<IFormFile> files
-    });
-
-    return this.http.post(
-      this.apiUrl + 'uploaddocumentfiles',
-      form
-    );
-  }
-
-
   // lưu supplier Sale
   saveSupplierSale(
     supplierSale: any,
@@ -139,29 +52,25 @@ export class SupplierSaleService {
       this.apiUrl + `supplier-sale`, supplierSale
     );
   }
-  // lưu document file
-  saveDocumentFile(
-    documentfile: any,
-  ): Observable<any> {
-    return this.http.post<any>(
-      this.apiUrl + `savedocumentfile`, documentfile
-    );
-  }
-
   // lưu supplier Sale Contact
   saveSupplierSaleContact(
     supplierSaleContact: any,
   ): Observable<any> {
     return this.http.post<any>(
-      this.apiUrl + `supplier-sale-contact`, supplierSaleContact
+      this.apiUrlSupplierSaleContact + `supplier-sale-contact`, supplierSaleContact
     );
   }
-  // lưu document
-  saveDocument(
-    document: any,
-  ): Observable<any> {
-    return this.http.post<any>(
-      this.apiUrl + `savedocument`, document
+
+  getSupplierSaleByID(supplierSaleID: number): Observable<any> {
+    return this.http.get<any>(
+      this.apiUrl + `supplier-sale-by-id?supplierID=${supplierSaleID}`,
     );
   }
+
+  getRulePay(): Observable<any> {
+    return this.http.get<any>(
+      this.apiUrlRulePay + `rule-pay`
+    );
+  }
+  
 }

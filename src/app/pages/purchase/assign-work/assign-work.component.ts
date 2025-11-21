9b80c863db5a5d86bafd1203c17d167a050ddfdf
TuNam <vutunam.rtc@gmail.com>
2025-11-21 19:42:30 +0700

    Update chức năng phân công công việc - module mua hàng

---
+++ src/app/pages/purchase/assign-work/assign-work.component.ts
@@ -0,0 +1,249 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NzButtonModule, NzButtonSize } from 'ng-zorro-antd/button';
+import { NzSplitterModule } from 'ng-zorro-antd/splitter';
+import { TabulatorFull as Tabulator } from 'tabulator-tables';
+import 'tabulator-tables/dist/css/tabulator_simple.min.css';
+import { OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { ApplicationRef, createComponent, Type } from '@angular/core';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
+import { DateTime } from 'luxon';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzTreeSelectModule } from 'ng-zorro-antd/tree-select';
+import * as ExcelJS from 'exceljs';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { NzModalModule, NzModalService } from 'ng-zorro-antd/modal';
+import { NgModel } from '@angular/forms';
+import { NgbModal, NgbNavOutlet } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router';
+import { HasPermissionDirective } from '../../../directives/has-permission.directive';
+import { CommonModule } from '@angular/common';
+import { NOTIFICATION_TITLE } from '../../../app.config';
+import { AssignWorkService } from './assign-work.service';
+import { ChooseEmployeeComponent } from './choose-employee/choose-employee.component';
+
+@Component({
+  selector: 'app-assign-work',
+  imports: [
+    FormsModule,
+    NzButtonModule,
+    NzSplitterModule,
+    NzTabsModule,
+    NzSpinModule,
+    NzTreeSelectModule,
+    NzModalModule,
+    CommonModule,
+    NzButtonModule,
+    HasPermissionDirective
+  ],
+  templateUrl: './assign-work.component.html',
+  styleUrl: './assign-work.component.css'
+})
+export class AssignWorkComponent implements OnInit, AfterViewInit {
+
+  //#region Khai báo biến
+  constructor(
+    private notification: NzNotificationService,
+    private modalService: NgbModal,
+    private modal: NzModalService,
+    private assignWorkService: AssignWorkService,
+  ) { }
+
+  @ViewChild('tb_userTeamLink', { static: false })
+  tb_userTeamLinkContainer!: ElementRef;
+  @ViewChild('tb_userTeamLinkDetail', { static: false })
+  tb_userTeamLinkDetailContainer!: ElementRef;
+
+  tb_userTeamLink: any;
+  tb_userTeamLinkDetail: any;
+
+  projectTypeId: any = 0;
+  //#endregion
+
+  //#region Hàm chạy khi mở chương trình
+  ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+    this.drawTbUserTeamLink(this.tb_userTeamLinkContainer.nativeElement);
+    this.drawTbUserTeamLinkDetail(this.tb_userTeamLinkDetailContainer.nativeElement);
+    this.getProjectType();
+    this.getProjectTypeAssign(this.projectTypeId);
+  }
+  //#endregion
+
+  //#region Tạo bảng 
+  drawTbUserTeamLink(container: HTMLElement) {
+    this.tb_userTeamLink = new Tabulator(container, {
+      height: '89vh',
+      layout: 'fitDataStretch',
+      dataTree: true,
+      dataTreeStartExpanded: true,
+      selectableRows: 1,
+      locale: 'vi',
+      dataTreeChildField: '_children',
+      columns: [
+        {
+          title: 'Mã',
+          field: 'ProjectTypeCode',
+          width: 150,
+          headerSort: false
+        },
+        {
+          title: 'Tên',
+          field: 'ProjectTypeName',
+          width: 150,
+          headerSort: false
+        },
+      ],
+    });
+
+    this.tb_userTeamLink.on('rowSelected', (row: any) => {
+      const id = row.getData().ID;
+      this.projectTypeId = id;
+      this.getProjectTypeAssign(id);
+    });
+  }
+
+  drawTbUserTeamLinkDetail(container: HTMLElement) {
+    this.tb_userTeamLinkDetail = new Tabulator(container, {
+      height: '89vh',
+      layout: 'fitDataStretch',
+      selectableRows: true,
+      locale: 'vi',
+      groupBy: (data) => `Danh mục: ${data.ProjectTypeName ?? ""}`,
+      groupHeader: function (value, count, data, group) {
+        return `${value}`;
+      },
+      columns: [
+        {
+          title: '',
+          field: '',
+          headerHozAlign: 'center',
+          formatter: 'rowSelection',
+          titleFormatter: 'rowSelection',
+          hozAlign: 'center',
+          headerSort: false,
+          width: 50,
+          cellClick: function (e, cell) {
+            e.stopPropagation();
+          },
+        },
+        {
+          title: 'Mã nhân viên',
+          field: 'Code',
+          width: 150,
+          headerSort: false
+        },
+        {
+          title: 'Tên nhân viên',
+          field: 'FullName',
+          width: 150,
+          headerSort: false
+        },
+      ],
+    });
+  }
+
+  //#endregion
+
+  //#region Set data tree
+  setDataTree(flatData: any[], valueField: string): any[] {
+    const map = new Map<number, any>();
+    const tree: any[] = [];
+
+    // Bước 1: Map từng item theo ID
+    flatData.forEach((item) => {
+      map.set(item[valueField], { ...item, _children: [] });
+    });
+
+    // Bước 2: Gắn item vào parent hoặc top-level
+    flatData.forEach((item) => {
+      const current = map.get(item[valueField]);
+      if (item.ID != 0) {
+        const parent = map.get(item.ParentID);
+        if (parent) {
+          parent._children.push(current);
+        } else {
+          tree.push(current);
+        }
+      } else {
+        tree.push(current);
+      }
+    });
+
+    return tree;
+  }
+  //#endregion
+
+  //#region Lấy dữ liệu master detail
+  getProjectType() {
+    this.tb_userTeamLink.clearData();
+    this.assignWorkService.getProjectType().subscribe({
+      next: (data) => {
+        const newItem = {
+          ID: 0,
+          ParentID: 0,
+          ProjectTypeName: '--Tất cả--'
+        };
+        const newData = [newItem, ...data.data];
+        let tree = this.setDataTree(newData, 'ID');
+        this.tb_userTeamLink.setData(tree);
+        this.tb_userTeamLink.selectRow(0);
+      },
+      error: (error) => {
+        this.notification.error(
+          NOTIFICATION_TITLE.error,
+          error.error.message
+        );
+      }
+    });
+  }
+
+  getProjectTypeAssign(projectTypeID: number) {
+    this.tb_userTeamLinkDetail.clearData();
+    this.assignWorkService.getProjectTypeAssign(projectTypeID).subscribe({
+      next: (data) => {
+        this.tb_userTeamLinkDetail.setData(data.data);
+      },
+      error: (error) => {
+        this.notification.error(
+          NOTIFICATION_TITLE.error,
+          error.error.message
+        );
+      }
+    });
+  }
+  //#endregion
+
+  //#region Reset data
+  loadData() {
+    this.getProjectType();
+    this.getProjectTypeAssign(0);
+  }
+  //#endregion
+
+  //#region Thêm nhân viên
+  onAddEmployee() {
+    if (this.projectTypeId <= 0 || this.projectTypeId == null) {
+      this.notification.error(
+        NOTIFICATION_TITLE.error,
+        "Vui lòng chọn nhóm cần thêm nhân viên!"
+      );
+      return;
+    }
+    const modalRef = this.modalService.open(ChooseEmployeeComponent, {
+      centered: true,
+      size: 'lg',
+      backdrop: 'static',
+      keyboard: false,
+    });
+    modalRef.componentInstance.projectTypeId = this.projectTypeId;
+    modalRef.result.catch((reason) => {
+      this.getProjectTypeAssign(this.projectTypeId);
+    });
+  }
+  //#endregion
+
+}
---
+++ src/app/pages/purchase/assign-work/choose-employee/choose-employee.component.ts
@@ -0,0 +1,174 @@
+import { AfterViewInit, Component, ElementRef, Input, input, OnInit, ViewChild } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { AssignWorkService } from '../assign-work.service';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { ProjectService } from '../../../project/project-service/project.service';
+import { CommonModule } from '@angular/common';
+import { TabulatorFull as Tabulator } from 'tabulator-tables';
+import 'tabulator-tables/dist/css/tabulator_simple.min.css';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { DepartmentServiceService } from '../../../hrm/department/department-service/department-service.service';
+import { NOTIFICATION_TITLE } from '../../../../app.config';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-choose-employee',
+  imports: [
+    CommonModule,
+    NzButtonModule,
+    NzFormModule,
+    NzSelectModule,
+    FormsModule
+  ],
+  templateUrl: './choose-employee.component.html',
+  styleUrl: './choose-employee.component.css'
+})
+export class ChooseEmployeeComponent implements OnInit, AfterViewInit {
+  constructor(
+    public activeModal: NgbActiveModal,
+    private assignWorkService: AssignWorkService,
+    private notification: NzNotificationService,
+    private projectService: ProjectService,
+    private departmentService: DepartmentServiceService
+  ) { }
+
+  @Input() projectTypeId: any;
+  @ViewChild('tb_employee', { static: false })
+  tb_employeeContainer!: ElementRef;
+  tb_employee: any;
+  employees: any[] = [];
+  departments: any[] = [];
+  departmentId: any = 0;
+  keyword: any = "";
+
+  ngOnInit(): void {
+    this.getDepartments();
+  }
+
+  ngAfterViewInit(): void {
+    this.drawTbEmployee(this.tb_employeeContainer.nativeElement);
+    this.getEmployee();
+  }
+
+  onSubmit() {
+    const selectRows = this.tb_employee.getSelectedRows();
+
+    if (!selectRows || selectRows.length === 0) {
+      this.notification.error(
+        NOTIFICATION_TITLE.error,
+        'Vui lòng chọn ít nhất một nhân viên cần thêm'
+      );
+      return;
+    }
+
+    const selectedIds = selectRows.map((x: any) => x.ID);
+
+    this.assignWorkService.addEmployees(selectedIds).subscribe({
+      next: (response: any) => {
+      },
+      error: (error) => {
+        this.notification.error(
+          NOTIFICATION_TITLE.error,
+          error.error.message
+        );
+      },
+    });
+
+  }
+
+  getEmployee() {
+    this.assignWorkService.getEmployee(this.departmentId ?? 0).subscribe({
+      next: (response: any) => {
+        debugger
+        this.employees = response.data
+        this.tb_employee.setData(response.data);
+      },
+      error: (error) => {
+        this.notification.error(
+          NOTIFICATION_TITLE.error,
+          'Lỗi khi tải danh sách phòng ban: ' + error.error.message
+        );
+      },
+    });
+  }
+
+  getDepartments() {
+    this.departmentService.getDepartments().subscribe({
+      next: (data: any) => {
+        this.departments = data.data;
+      },
+      error: (error) => {
+        this.notification.error(
+          NOTIFICATION_TITLE.error,
+          'Lỗi khi tải danh sách phòng ban: ' + error.error.message
+        );
+      },
+    });
+  }
+
+  drawTbEmployee(container: HTMLElement) {
+    this.tb_employee = new Tabulator(container, {
+      height: '64vh',
+      layout: 'fitDataStretch',
+      selectableRows: true,
+      locale: 'vi',
+      groupBy: (data) => `Phòng ban: ${data.DepartmentName ?? ""}`,
+      groupHeader: function (value, count, data, group) {
+        return `${value}`;
+      },
+      columns: [
+        {
+          title: '',
+          field: '',
+          headerHozAlign: 'center',
+          formatter: 'rowSelection',
+          titleFormatter: 'rowSelection',
+          hozAlign: 'center',
+          headerSort: false,
+          width: 50,
+          cellClick: function (e, cell) {
+            e.stopPropagation();
+          },
+        },
+        {
+          title: 'Mã nhân viên',
+          field: 'Code',
+          width: 100,
+          headerSort: false
+        },
+        {
+          title: 'Tên nhân viên',
+          field: 'FullName',
+          width: 250,
+          headerSort: false
+        },
+        {
+          title: 'Chức vụ',
+          field: 'ChucVuHD',
+          width: 100,
+          headerSort: false
+        },
+      ],
+    });
+  }
+
+  onSearchEmployee(event: any) {
+    const keyword = event.target.value.toLowerCase();
+    if (keyword != "") {
+      this.tb_employee.setFilter([
+        [
+          { field: 'Code', type: 'like', value: keyword },
+          { field: 'FullName', type: 'like', value: keyword },
+          { field: 'ChucVuHD', type: 'like', value: keyword },
+        ]
+      ], 'or'); // 'or' để tìm ở bất kỳ cột nào
+    } else {
+      this.drawTbEmployee(this.tb_employeeContainer.nativeElement);
+      this.getEmployee();
+    }
+  }
+
+
+}

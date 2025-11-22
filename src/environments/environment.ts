e0842827e092e49a43a9fa40027f041e03b6f19f
Admin <Admin@DESKTOP-40H717B>
2025-11-22 14:23:38 +0700

    Thêm env

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  apiKey:
+    "RTC%$#tEch~`'3keYRTC%$#tEch~`'3keYRTC%$#tEch~`'3keYRTC%$#tEch~`'3keYRTC%$#tEch~`'3keY",
+  host: 'https://localhost:44365/',
+  // host: 'http://10.20.28.238:8087/',
+  hostwebold: 'https://localhost:19028/',
+};

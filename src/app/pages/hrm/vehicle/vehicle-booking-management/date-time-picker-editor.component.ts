4056c663f1c3bce3f9ddb3b48c97065d54264b54
luongxuan1406 <luongtu1112@gmail.com>
2025-11-29 20:17:04 +0700

    Update chức năng cập nhật giờ XP thực tế, tiền xe trên view

---
+++ src/app/pages/hrm/vehicle/vehicle-booking-management/date-time-picker-editor.component.ts
@@ -0,0 +1,58 @@
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { DateTime } from 'luxon';
+
+@Component({
+  selector: 'app-date-time-picker-editor',
+  standalone: true,
+  imports: [CommonModule, FormsModule, NzDatePickerModule],
+  template: `
+    <nz-date-picker
+      [(ngModel)]="dateValue"
+      [nzShowTime]="true"
+      [nzFormat]="'dd/MM/yyyy HH:mm'"
+      nzSize="small"
+      style="width: 100%"
+      (ngModelChange)="onDateChange($event)"
+      (nzOnOpenChange)="onOpenChange($event)"
+    ></nz-date-picker>
+  `,
+  styles: [`
+    :host {
+      display: block;
+      width: 100%;
+    }
+  `]
+})
+export class DateTimePickerEditorComponent implements OnInit {
+  @Input() value: Date | string | null = null;
+  @Output() valueChange = new EventEmitter<Date | null>();
+  @Output() closeEditor = new EventEmitter<void>();
+
+  dateValue: Date | null = null;
+
+  ngOnInit() {
+    if (this.value) {
+      this.dateValue = this.value instanceof Date ? this.value : new Date(this.value);
+    }
+  }
+
+  onDateChange(date: Date | null) {
+    this.dateValue = date;
+    if (date) {
+      this.valueChange.emit(date);
+    }
+  }
+
+  onOpenChange(open: boolean) {
+    if (!open) {
+      // Khi đóng picker, emit close event
+      setTimeout(() => {
+        this.closeEditor.emit();
+      }, 100);
+    }
+  }
+}
+

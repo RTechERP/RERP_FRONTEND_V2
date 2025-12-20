ec6907ccea60629a3f533806829202e4919d9aa4
Admin <Admin@DESKTOP-40H717B>
2025-12-20 16:02:01 +0700

    update lại F5 khi hết token

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -5,7 +5,7 @@ import { catchError, throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
     //   console.log('[Interceptor]', req.url);
-
+    let isLoggingOut = false;
     const auth = inject(AuthService);
     const token = auth.getToken();
     // Không chèn token nếu là request tới API login
@@ -28,11 +28,24 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
     return next(req).pipe(
         catchError((error: HttpErrorResponse) => {
             // Session hết hạn / token invalid
-            if (error.status === 401 || error.status === 419) {
-                auth.logout(); // optional: clear token, user info
-                location.reload();
-                // hoặc: window.location.href = '/login';
+            // if (error.status === 401 || error.status === 419) {
+            //     auth.logout(); // optional: clear token, user info
+            //     location.reload();
+            //     // hoặc: window.location.href = '/login';
+            // }
+
+            if ((error.status === 401 || error.status === 419) && !isLoggingOut) {
+                isLoggingOut = true;
+
+                auth.logout(); // clear token, user
+
+                // ❌ KHÔNG reload
+                // location.reload();
+
+                // ✅ navigate về login
+                window.location.href = '/login';
             }
+
             return throwError(() => error);
         })
     );

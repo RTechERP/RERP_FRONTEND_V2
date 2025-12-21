97a3918b63527b066eadbc1aee4c71861a23927a
luongxuan1406 <luongtu1112@gmail.com>
2025-12-21 12:51:42 +0700

    Update fommat giờ bccv HR

--- src/app/pages/hrm/daily-report-hr/daily-report-hr.component.ts
+++ src/app/pages/hrm/daily-report-hr/daily-report-hr.component.ts
@@ -129,25 +129,16 @@ export class DailyReportHrComponent implements OnInit, AfterViewInit {
     });
   }
   getDailyReportHr(): void {
-
-    const formatDateStart = (date: Date | null): string => {
+    const formatDate = (date: Date | null): string => {
       if (!date) return '';
       const year = date.getFullYear();
       const month = String(date.getMonth() + 1).padStart(2, '0');
       const day = String(date.getDate()).padStart(2, '0');
-      return `${year}-${month}-${day}T00:00:00`;
+      return `${year}-${month}-${day}`;
     };
-    const formatDateEnd = (date: Date | null): string => {
-      if (!date) return '';
-      const year = date.getFullYear();
-      const month = String(date.getMonth() + 1).padStart(2, '0');
-      const day = String(date.getDate()).padStart(2, '0');
-      return `${year}-${month}-${day}T23:59:59`;
-    };
-
     const payload = {
-      dateStart: formatDateStart(this.DateStart),
-      dateEnd: formatDateEnd(this.DateEnd),
+      dateStart: formatDate(this.DateStart),
+      dateEnd: formatDate(this.DateEnd),
       keyword: this.Keyword || '',
       departmentID: this.DepartmentID || 0,
       userID: this.UserID || 0,
@@ -158,11 +149,6 @@ export class DailyReportHrComponent implements OnInit, AfterViewInit {
         this.filmReport = res.data.dataFilm || [];
         this.driverReport = res.data.dataDriver || [];
         this.hrReport = res.data.technical || [];
-
-        console.log('filmReport', this.filmReport);
-        console.log('driverReport', this.driverReport);
-        console.log('hrReport', this.hrReport);
-
         this.initOrUpdateTables();
       },
       error: (err: any) => {

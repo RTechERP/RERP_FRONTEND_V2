76d187f22bd5749d3ce325063a1ef15f64390feb
Nam <vutunam.rtc@gmail.com>
2026-01-07 11:19:10 +0700

    Update bổ sung chọn serial phiếu nhập xuất sale/demo

--- src/app/pages/old/bill-export-technical/bill-export-technical-form/bill-export-technical-form.component.ts
+++ src/app/pages/old/bill-export-technical/bill-export-technical-form/bill-export-technical-form.component.ts
@@ -1353,6 +1353,14 @@ export class BillExportTechnicalFormComponent implements OnInit, AfterViewInit {
     productCode: string,
     existingSerials: { ID: number; Serial: string }[]
   ) {
+    if (rowData.ID == null || rowData.ID <= 0) {
+      this.notification.warning(
+        NOTIFICATION_TITLE.warning,
+        'Các mã sản phẩm thêm mới cần lưu trước khi chọn serial!'
+      );
+      return;
+    }
+    console.log(rowData);
     const modalRef = this.ngbModal.open(BillImportChoseSerialComponent, {
       size: 'md',
       centered: true,

0fada609246a040285db65f9f03bc96c7268bb22
luongxuan1406 <luongtu1112@gmail.com>
2026-03-06 13:55:59 +0700

    Sửa FieldName đăng kí công tác nhân sự

--- src/app/pages/hrm/employee-management/employee-bussiness/employee-bussiness.component.ts
+++ src/app/pages/hrm/employee-management/employee-bussiness/employee-bussiness.component.ts
@@ -303,7 +303,7 @@ export class EmployeeBussinessComponent implements OnInit, AfterViewInit, OnChan
           title: 'Loại', field: 'TypeName', hozAlign: 'left', headerHozAlign: 'center', width: 220, headerSort: false,
         },
         {
-          title: 'Dự án', field: 'Project', hozAlign: 'left', headerHozAlign: 'center', width: 220, headerSort: false,
+          title: 'Dự án', field: 'ProjectName', hozAlign: 'left', headerHozAlign: 'center', width: 220, headerSort: false,
         },
         {
           title: 'Phương tiện', field: 'VehicleName', hozAlign: 'left', headerHozAlign: 'center', width: 200, headerSort: false,

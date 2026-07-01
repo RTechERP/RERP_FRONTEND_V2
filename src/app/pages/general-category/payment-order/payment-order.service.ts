0686cfa2a8ce77a94be093236542a89e7e424dcd
Admin <Admin@DESKTOP-40H717B>
2026-07-01 14:41:47 +0700

    thêm loại tiền franc

--- src/app/pages/general-category/payment-order/payment-order.service.ts
+++ src/app/pages/general-category/payment-order/payment-order.service.ts
@@ -5,278 +5,278 @@ import { PaymentOrder } from './model/payment-order';
 import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class PaymentOrderService {
 
-    private url = environment.host + 'api/paymentorder';
+  private url = environment.host + 'api/paymentorder';
 
-    // ====== 1. Danh sách chữ số ======
-    private ChuSo = [
-        'không',
-        'một',
-        'hai',
-        'ba',
-        'bốn',
-        'năm',
-        'sáu',
-        'bảy',
-        'tám',
-        'chín'
-    ];
+  // ====== 1. Danh sách chữ số ======
+  private ChuSo = [
+    'không',
+    'một',
+    'hai',
+    'ba',
+    'bốn',
+    'năm',
+    'sáu',
+    'bảy',
+    'tám',
+    'chín'
+  ];
 
-    // ====== 2. Đơn vị tiền ======
-    private currencyMap = new Map<string, CurrencyConfig>(
-        CURRENCY_CONFIGS.map(c => [c.id, c])
-    );
+  // ====== 2. Đơn vị tiền ======
+  private currencyMap = new Map<string, CurrencyConfig>(
+    CURRENCY_CONFIGS.map(c => [c.id, c])
+  );
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-    get(data: any): Observable<any> {
-        return this.http.post<any>(this.url, data);
+  get(data: any): Observable<any> {
+    return this.http.post<any>(this.url, data);
+  }
+  getPaging(data: any): Observable<any> {
+    return this.http.post<any>(`${this.url}/paging`, data);
+  }
+  getSpecial(data: any): Observable<any> {
+    return this.http.post<any>(`${this.url}/special`, data);
+  }
+
+  getDetail(id: number): Observable<any> {
+    return this.http.get<any>(this.url + `/${id}`);
+  }
+
+  deleteMulti(ids: number[]): Observable<any> {
+    return this.http.post<any>(`${this.url}/delete-multi`, ids);
+  }
+
+  getLogNew(paymentOrderId: number): Observable<any> {
+    return this.http.get<any>(`${environment.host}api/paymentorderlog/get-data-new`, { params: { paymentOrderId } });
+  }
+
+  save(payment: any): Observable<any> {
+    return this.http.post<any>(`${this.url}/save-data`, payment);
+  }
+  updateTotalmoney(payment: any): Observable<any> {
+    return this.http.post<any>(`${this.url}/update-totalmoney`, payment);
+  }
+
+  updateTransferType(payments: any[]): Observable<any> {
+    return this.http.post<any>(`${this.url}/update-transfer-type`, payments);
+  }
+
+  downloadZip(file: DownloadPaymentOrderDTO): Observable<Blob> {
+    return this.http.post(`${this.url}/download-zip`, file, { responseType: 'blob' });
+  }
+
+  uploadFile(files: File[], paymentOrderID: number, paymentOrderFileID: string): Observable<any> {
+    const formData = new FormData();
+    if (files) {
+      Array.from(files).forEach(file => {
+        formData.append('files', file);
+      });
     }
-    getPaging(data: any): Observable<any> {
-        return this.http.post<any>(`${this.url}/paging`, data);
-    }
-    getSpecial(data: any): Observable<any> {
-        return this.http.post<any>(`${this.url}/special`, data);
+
+    formData.append('PaymentOrderID', paymentOrderID.toString());
+    formData.append('PaymentOrderFile', paymentOrderFileID.toString());
+    return this.http.post<any>(`${this.url}/upload-file`, formData);
+  }
+  // Thêm vào payment-order.service.ts
+  getDataFromPONCC(ponccID: number): Observable<any> {
+    return this.http.get<any>(`${this.url}/get-data-from-poncc/${ponccID}`);
+  }
+  uploadFileBankslip(files: File[], paymentOrderID: string): Observable<any> {
+    const formData = new FormData();
+    if (files) {
+      Array.from(files).forEach(file => {
+        formData.append('files', file);
+      });
     }
 
-    getDetail(id: number): Observable<any> {
-        return this.http.get<any>(this.url + `/${id}`);
+    formData.append('PaymentOrderID', paymentOrderID);
+    return this.http.post<any>(`${this.url}/upload-file-bankslip`, formData);
+  }
+
+  appovedTBP(data: any): Observable<any> {
+    return this.http.post<any>(`${this.url}/appoved-tbp`, data);
+  }
+
+  appovedHR(data: any): Observable<any> {
+    return this.http.post<any>(`${this.url}/appoved-hr`, data);
+  }
+
+  appovedKTTT(data: any): Observable<any> {
+    return this.http.post<any>(`${this.url}/appoved-kttt`, data);
+  }
+
+  appovedKTT(data: any): Observable<any> {
+    return this.http.post<any>(`${this.url}/appoved-ktt`, data);
+  }
+
+  appovedBGD(data: any): Observable<any> {
+    return this.http.post<any>(`${this.url}/appoved-bgd`, data);
+  }
+
+  appovedKHReceive(data: any): Observable<any> {
+    return this.http.post<any>(`${this.url}/appoved-khreceive`, data);
+  }
+
+  approved(data: any): Observable<any> {
+    return this.http.post<any>(`${this.url}/approved`, data);
+  }
+
+  uploadFiles(file: any): Observable<any> {
+    return this.http.post<any>(`${this.url}/upload-file`, file);
+  }
+
+  getDataCombo(): Observable<any> {
+    return this.http.get<any>(`${this.url}/get-data-combo`);
+  }
+
+  getApprovers(): Observable<any> {
+    return this.http.get<any>(`${this.url}/approvers`);
+  }
+
+  getProcurement(): Observable<any> {
+    return this.http.get<any>(`${this.url}/procurement`);
+  }
+
+  getPartnersAndProjects(): Observable<any> {
+    return this.http.get<any>(`${this.url}/partners-projects`);
+  }
+
+  getMetadata(): Observable<any> {
+    return this.http.get<any>(`${this.url}/metadata`);
+  }
+
+  getStep(): Observable<any> {
+    return this.http.get<any>(`${this.url}/get-step`);
+  }
+
+  getBankList(): Observable<any> {
+    return this.http.get<any>(`${this.url}/get-bank-list`);
+  }
+  // ====== 4. Đọc 3 chữ số ======
+  private readThreeDigits(num: number): string {
+    let tram = Math.floor(num / 100);
+    let chuc = Math.floor((num % 100) / 10);
+    let donvi = num % 10;
+    let result = '';
+
+    if (tram > 0) {
+      result += this.ChuSo[tram] + ' trăm';
+      if (chuc === 0 && donvi > 0) result += ' lẻ';
     }
 
-    deleteMulti(ids: number[]): Observable<any> {
-        return this.http.post<any>(`${this.url}/delete-multi`, ids);
+    if (chuc > 1) {
+      result += ' ' + this.ChuSo[chuc] + ' mươi';
+      if (donvi === 1) result += ' mốt';
+      else if (donvi === 5) result += ' lăm';
+      else if (donvi > 0) result += ' ' + this.ChuSo[donvi];
+    } else if (chuc === 1) {
+      result += ' mười';
+      if (donvi === 1) result += ' một';
+      else if (donvi === 5) result += ' lăm';
+      else if (donvi > 0) result += ' ' + this.ChuSo[donvi];
+    } else if (chuc === 0 && donvi > 0) {
+      // result += this.ChuSo[donvi];
+      result += ' ' + this.ChuSo[donvi];
     }
 
-    getLogNew(paymentOrderId: number): Observable<any> {
-        return this.http.get<any>(`${environment.host}api/paymentorderlog/get-data-new`, { params: { paymentOrderId } });
-    }
+    return result.trim();
+  }
 
-    save(payment: any): Observable<any> {
-        return this.http.post<any>(`${this.url}/save-data`, payment);
-    }
-    updateTotalmoney(payment: any): Observable<any> {
-        return this.http.post<any>(`${this.url}/update-totalmoney`, payment);
-    }
+  // ====== 5. HÀM CHÍNH đọc tiền ======
+  readMoney(value: number | string, currencyId: string): string {
+    const amount = Number(value);
+    if (isNaN(amount)) return '';
 
-    updateTransferType(payments: any[]): Observable<any> {
-        return this.http.post<any>(`${this.url}/update-transfer-type`, payments);
-    }
+    const currency = this.currencyMap.get(currencyId.toLowerCase());
+    if (!currency) return '';
 
-    downloadZip(file: DownloadPaymentOrderDTO): Observable<Blob> {
-        return this.http.post(`${this.url}/download-zip`, file, { responseType: 'blob' });
-    }
+    const integerPart = Math.floor(amount);
+    const decimalPart = Math.round((amount - integerPart) * 100);
 
-    uploadFile(files: File[], paymentOrderID: number, paymentOrderFileID: string): Observable<any> {
-        const formData = new FormData();
-        if (files) {
-            Array.from(files).forEach(file => {
-                formData.append('files', file);
-            });
+    let result = '';
+
+    // --- phần nguyên
+    if (integerPart === 0) {
+      result = 'Không ' + currency.unit;
+    } else {
+      let temp = integerPart;
+      let i = 0;
+      const units = ['', ' nghìn', ' triệu', ' tỷ', ' nghìn tỷ', ' triệu tỷ', ' tỷ tỷ'];
+      let text = '';
+
+      while (temp > 0) {
+        const block = temp % 1000;
+        if (block > 0) {
+          text =
+            this.readThreeDigits(block) +
+            units[i] +
+            (text ? ' ' + text : '');
         }
+        temp = Math.floor(temp / 1000);
+        i++;
+      }
 
-        formData.append('PaymentOrderID', paymentOrderID.toString());
-        formData.append('PaymentOrderFile', paymentOrderFileID.toString());
-        return this.http.post<any>(`${this.url}/upload-file`, formData);
-    }
-    // Thêm vào payment-order.service.ts
-    getDataFromPONCC(ponccID: number): Observable<any> {
-        return this.http.get<any>(`${this.url}/get-data-from-poncc/${ponccID}`);
-    }
-    uploadFileBankslip(files: File[], paymentOrderID: string): Observable<any> {
-        const formData = new FormData();
-        if (files) {
-            Array.from(files).forEach(file => {
-                formData.append('files', file);
-            });
-        }
 
-        formData.append('PaymentOrderID', paymentOrderID);
-        return this.http.post<any>(`${this.url}/upload-file-bankslip`, formData);
+
+
+      result =
+        text.charAt(0).toUpperCase() +
+        text.slice(1) +
+        ' ' +
+        currency.unit;
     }
 
-    appovedTBP(data: any): Observable<any> {
-        return this.http.post<any>(`${this.url}/appoved-tbp`, data);
+    // --- phần lẻ
+    if (decimalPart > 0 && currency.subUnit) {
+      result +=
+        ' và ' +
+        this.readThreeDigits(decimalPart) +
+        ' ' +
+        currency.subUnit;
     }
 
-    appovedHR(data: any): Observable<any> {
-        return this.http.post<any>(`${this.url}/appoved-hr`, data);
+    return result;
+  }
+
+
+  getTeamSale(employeeId: number): Observable<any> {
+    return this.http.get<any>(`${this.url}/get-team-sale/${employeeId}`);
+  }
+
+  getPaymentOrderByTeam(data: any): Observable<any> {
+    return this.http.post<any>(`${this.url}/get-payment-order-team`, data);
+  }
+  getCurrencyConfig(): Observable<CurrencyConfig> {
+    return this.http.get<any>(`${this.url}/get-currency-config`);
+  }
+
+  updatePaymentOrderSupplierAndInvoice(paymentID: number, typeUpdate: number, contentUpdate: string): Observable<any> {
+    let httpParams = new HttpParams();
+
+    if (paymentID !== undefined && paymentID !== null) {
+      httpParams = httpParams.set('paymentID', paymentID.toString());
+    } else {
+      httpParams = httpParams.set('paymentID', '');
     }
 
-    appovedKTTT(data: any): Observable<any> {
-        return this.http.post<any>(`${this.url}/appoved-kttt`, data);
+    if (typeUpdate !== undefined && typeUpdate !== null) {
+      httpParams = httpParams.set('typeUpdate', typeUpdate.toString());
+    } else {
+      httpParams = httpParams.set('typeUpdate', '');
     }
 
-    appovedKTT(data: any): Observable<any> {
-        return this.http.post<any>(`${this.url}/appoved-ktt`, data);
+    if (contentUpdate !== undefined && contentUpdate !== null) {
+      httpParams = httpParams.set('contentUpdate', contentUpdate.toString());
+    } else {
+      httpParams = httpParams.set('contentUpdate', '');
     }
 
-    appovedBGD(data: any): Observable<any> {
-        return this.http.post<any>(`${this.url}/appoved-bgd`, data);
-    }
-
-    appovedKHReceive(data: any): Observable<any> {
-        return this.http.post<any>(`${this.url}/appoved-khreceive`, data);
-    }
-
-    approved(data: any): Observable<any> {
-        return this.http.post<any>(`${this.url}/approved`, data);
-    }
-
-    uploadFiles(file: any): Observable<any> {
-        return this.http.post<any>(`${this.url}/upload-file`, file);
-    }
-
-    getDataCombo(): Observable<any> {
-        return this.http.get<any>(`${this.url}/get-data-combo`);
-    }
-
-    getApprovers(): Observable<any> {
-        return this.http.get<any>(`${this.url}/approvers`);
-    }
-
-    getProcurement(): Observable<any> {
-        return this.http.get<any>(`${this.url}/procurement`);
-    }
-
-    getPartnersAndProjects(): Observable<any> {
-        return this.http.get<any>(`${this.url}/partners-projects`);
-    }
-
-    getMetadata(): Observable<any> {
-        return this.http.get<any>(`${this.url}/metadata`);
-    }
-
-    getStep(): Observable<any> {
-        return this.http.get<any>(`${this.url}/get-step`);
-    }
-
-    getBankList(): Observable<any> {
-        return this.http.get<any>(`${this.url}/get-bank-list`);
-    }
-    // ====== 4. Đọc 3 chữ số ======
-    private readThreeDigits(num: number): string {
-        let tram = Math.floor(num / 100);
-        let chuc = Math.floor((num % 100) / 10);
-        let donvi = num % 10;
-        let result = '';
-
-        if (tram > 0) {
-            result += this.ChuSo[tram] + ' trăm';
-            if (chuc === 0 && donvi > 0) result += ' lẻ';
-        }
-
-        if (chuc > 1) {
-            result += ' ' + this.ChuSo[chuc] + ' mươi';
-            if (donvi === 1) result += ' mốt';
-            else if (donvi === 5) result += ' lăm';
-            else if (donvi > 0) result += ' ' + this.ChuSo[donvi];
-        } else if (chuc === 1) {
-            result += ' mười';
-            if (donvi === 1) result += ' một';
-            else if (donvi === 5) result += ' lăm';
-            else if (donvi > 0) result += ' ' + this.ChuSo[donvi];
-        } else if (chuc === 0 && donvi > 0) {
-            // result += this.ChuSo[donvi];
-            result += ' ' + this.ChuSo[donvi];
-        }
-
-        return result.trim();
-    }
-
-    // ====== 5. HÀM CHÍNH đọc tiền ======
-    readMoney(value: number | string, currencyId: string): string {
-        const amount = Number(value);
-        if (isNaN(amount)) return '';
-
-        const currency = this.currencyMap.get(currencyId.toLowerCase());
-        if (!currency) return '';
-
-        const integerPart = Math.floor(amount);
-        const decimalPart = Math.round((amount - integerPart) * 100);
-
-        let result = '';
-
-        // --- phần nguyên
-        if (integerPart === 0) {
-            result = 'Không ' + currency.unit;
-        } else {
-            let temp = integerPart;
-            let i = 0;
-            const units = ['', ' nghìn', ' triệu', ' tỷ', ' nghìn tỷ', ' triệu tỷ', ' tỷ tỷ'];
-            let text = '';
-
-            while (temp > 0) {
-                const block = temp % 1000;
-                if (block > 0) {
-                    text =
-                        this.readThreeDigits(block) +
-                        units[i] +
-                        (text ? ' ' + text : '');
-                }
-                temp = Math.floor(temp / 1000);
-                i++;
-            }
-
-
-
-
-            result =
-                text.charAt(0).toUpperCase() +
-                text.slice(1) +
-                ' ' +
-                currency.unit;
-        }
-
-        // --- phần lẻ
-        if (decimalPart > 0 && currency.subUnit) {
-            result +=
-                ' và ' +
-                this.readThreeDigits(decimalPart) +
-                ' ' +
-                currency.subUnit;
-        }
-
-        return result;
-    }
-
-
-    getTeamSale(employeeId: number): Observable<any> {
-        return this.http.get<any>(`${this.url}/get-team-sale/${employeeId}`);
-    }
-
-    getPaymentOrderByTeam(data: any): Observable<any> {
-        return this.http.post<any>(`${this.url}/get-payment-order-team`, data);
-    }
-    getCurrencyConfig(): Observable<CurrencyConfig> {
-        return this.http.get<any>(`${this.url}/get-currency-config`);
-    }
-
-    updatePaymentOrderSupplierAndInvoice(paymentID: number, typeUpdate: number, contentUpdate: string): Observable<any> {
-        let httpParams = new HttpParams();
-
-        if (paymentID !== undefined && paymentID !== null) {
-            httpParams = httpParams.set('paymentID', paymentID.toString());
-        } else {
-            httpParams = httpParams.set('paymentID', '');
-        }
-
-        if (typeUpdate !== undefined && typeUpdate !== null) {
-            httpParams = httpParams.set('typeUpdate', typeUpdate.toString());
-        } else {
-            httpParams = httpParams.set('typeUpdate', '');
-        }
-
-        if (contentUpdate !== undefined && contentUpdate !== null) {
-            httpParams = httpParams.set('contentUpdate', contentUpdate.toString());
-        } else {
-            httpParams = httpParams.set('contentUpdate', '');
-        }
-
-        return this.http.get<any>(`${this.url}/update-supplier-code-and-invoice-number`, { params: httpParams });
-    }
+    return this.http.get<any>(`${this.url}/update-supplier-code-and-invoice-number`, { params: httpParams });
+  }
 
 }
 
@@ -284,27 +284,28 @@ export class PaymentOrderService {
 
 
 export interface CurrencyConfig {
-    id: string;          // vnd, usd...
-    text: string;        // VND, USD (hiển thị combo)
-    unit: string;        // đồng, đô la
-    subUnit?: string;    // xu, cent...
+  id: string;          // vnd, usd...
+  text: string;        // VND, USD (hiển thị combo)
+  unit: string;        // đồng, đô la
+  subUnit?: string;    // xu, cent...
 }
 
 export interface DownloadPaymentOrderDTO {
-    PaymentOrderId: number;
-    PaymentOrderCode: string;
-    FilePath: string[];
+  PaymentOrderId: number;
+  PaymentOrderCode: string;
+  FilePath: string[];
 }
 
 export const CURRENCY_CONFIGS: CurrencyConfig[] = [
-    { id: 'vnd', text: 'VND', unit: 'đồng', subUnit: 'xu' },
-    { id: 'usd', text: 'USD', unit: 'đô', subUnit: 'cent' },
-    { id: 'eur', text: 'EURO', unit: 'euro', subUnit: 'cent' },
-    { id: 'jpy', text: 'JPY', unit: 'yên', subUnit: 'sen' },
-    { id: 'sgd', text: 'SGD', unit: 'đô', subUnit: 'cent' },
-    { id: 'cny', text: 'CNY', unit: 'nhân dân tệ', subUnit: '' },
-    { id: 'inr', text: 'INR', unit: 'rupee', subUnit: 'paise' },
-    { id: 'myr', text: 'MYR', unit: 'ringgit', subUnit: 'sen' },
-    { id: 'idr', text: 'IDR', unit: 'rupiah', subUnit: 'sen' },
+  { id: 'vnd', text: 'VND', unit: 'đồng', subUnit: 'xu' },
+  { id: 'usd', text: 'USD', unit: 'đô', subUnit: 'cent' },
+  { id: 'eur', text: 'EURO', unit: 'euro', subUnit: 'cent' },
+  { id: 'jpy', text: 'JPY', unit: 'yên', subUnit: 'sen' },
+  { id: 'sgd', text: 'SGD', unit: 'đô', subUnit: 'cent' },
+  { id: 'cny', text: 'CNY', unit: 'nhân dân tệ', subUnit: '' },
+  { id: 'inr', text: 'INR', unit: 'rupee', subUnit: 'paise' },
+  { id: 'myr', text: 'MYR', unit: 'ringgit', subUnit: 'sen' },
+  { id: 'idr', text: 'IDR', unit: 'rupiah', subUnit: 'sen' },
+  { id: 'chf', text: 'CHF', unit: 'franc', subUnit: 'rappen' },
 
 ];
